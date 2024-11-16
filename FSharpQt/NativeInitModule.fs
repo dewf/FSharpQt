3f6f751f3927843d14e3060202cecef0b1b86218
Daniel X <nobody@nowhere.net>
2024-11-15 21:09:18 -0700

    whoops, forgot one

---
+++ FSharpQt/NativeInitModule.fs
@@ -0,0 +1,12 @@
+﻿module FSharpQt.NativeInitModule
+open Org.Whatever.MinimalQtForFSharp
+
+// ====================================================================================
+// any type that needs the native stuff ready to go (for top-level `let` bindings),
+// needs to call this in its static initializer (static do)
+// eg Color, Font, Pixmap, etc ...
+let mutable private loaded = false
+let ensureNativeLibraryLoaded() =
+    if not loaded then
+        Library.Init()
+        loaded <- true
